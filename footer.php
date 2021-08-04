65e303005d2d0aee213b3674046422d9ce4e451e
Joseph Chambers <joseph@michael-chambers.com>
2021-08-04 11:40:27 -0500

    Add header.php and footer.php for dokan lite
    fix error in plugin dokan lite:
    
    ErrorException (E_USER_DEPRECATED)
    Theme without header.php is <strong>deprecated</strong> since version 3.0.0 with no alternative available. Please include a header.php template in your theme.
    
    plugin dokan-lite requires these files to exist: header.php footer.php
    
    - Let's copy structure from index.php and layout/app
    - Then render blade views

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php 
+
+/* NOTE:
+ * plugin dokan-lite requires these files to exist: header.php footer.php
+ * Let's copy structure from index.php and layout/app
+ * Then render blade views
+ *
+ * */ 
+
+?>
+
+    </main>
+      <?php echo \Roots\view('partials.footer')->render(); ?>
+</div>
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<?php 
+
+/* NOTE:
+ * plugin dokan-lite requires these files to exist: header.php footer.php
+ * Let's copy structure from index.php and layout/app
+ * Then render blade views
+ *
+ * */ 
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+<div class="flex-1 flex flex-col relative bg-white">
+
+  <a class="sr-only focus:not-sr-only" href="#main">
+    {{ __('Skip to content') }}
+  </a>
+
+    <?php echo \Roots\view('partials.header')->render(); ?>
+
+    <main id="main" class="py-8 main max-w-6xl mx-auto flex-1 w-full">
