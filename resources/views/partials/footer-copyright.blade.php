58f8599732b356a8a0ea02eacf6ade55d6f38112
Joseph Chambers <joseph@michael-chambers.com>
2021-08-10 13:34:59 -0500

    Remove footer.css and footer-copyright
    Too small to be worth another file

--- resources/views/partials/footer-copyright.blade.php
+++
@@ -1,1 +0,0 @@
-<span class="text-white text-sm uppercase">{{ $siteName }} © {{ date('Y') }}</span>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,15 +2,15 @@
   <div class="container mx-auto px-6">
     <div class="flex flex-wrap space-y-6 md:flex-nowrap md:items-center md:space-y-0">
       @if (has_nav_menu('footer_navigation'))
-        <div class="footer__col">
+        <div class="flex w-full justify-center md:items-center md:space-x-6 md:mr-6 md:w-auto md:justify-start">
           @include('partials.footer-navigation')
         </div>
       @endif
-      <div class="footer__col md:ml-auto">
+      <div class="flex w-full justify-center md:items-center md:space-x-6 md:mr-6 md:w-auto md:justify-start md:ml-auto">
         @include('partials.footer-icons')
       </div>
-      <div class="footer__col">
-        @include('partials.footer-copyright')
+      <div class="flex w-full justify-center md:items-center md:space-x-6 md:mr-6 md:w-auto md:justify-start md:mr-0">
+        <span class="text-white text-sm uppercase">{{ $siteName }} © {{ date('Y') }}</span>
       </div>
     </div>
   </div>
