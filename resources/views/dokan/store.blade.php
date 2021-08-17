9b272a0597a5b79f20b419f9418840849cd1d7ce
Joseph Chambers <joseph@michael-chambers.com>
2021-08-16 22:12:49 -0500

    transform store to blade
    Transform php code to blade

--- resources/views/dokan/store.blade.php
+++ resources/views/dokan/store.blade.php
@@ -1,71 +1,41 @@
-<?php
-/**
- * The Template for displaying all single posts.
- *
- * @package dokan
- * @package dokan - 2014 1.0
- */
+{{-- The Template for displaying all single posts. --}}
+{{-- example: store/storename --}}
+@php get_header('shop') @endphp
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+<div class="dokan-store-wrap layout-{{ $layout }}">
 
-// change: parameters in app/View/Composers/DokanStore.php
-get_header( 'shop' );
-
-// change: no yoast_breadcrumb
-?>
-{{-- change: no woocommerce_before_main_content --}}
-
-<div class="dokan-store-wrap layout-<?php echo esc_attr( $layout ); ?>">
-
-    {{-- change: no left layout --}}
-
-    <div id="dokan-primary" class="dokan-single-store">
-        <div id="dokan-content" class="store-page-wrap woocommerce" role="main">
-
-            <?php dokan_get_template_part( 'store-header' ); ?>
-            {{-- change: --}} 
-            @include('dokan.store-header-seller')
-
-            <?php do_action( 'dokan_store_profile_frame_after', $store_user->data, $store_info ); ?>
-
-            <?php if ( have_posts() ) { ?>
-
-                <div class="seller-items">
-
-                    <?php woocommerce_product_loop_start(); ?>
-
-                    <?php while ( have_posts() ) : the_post(); ?>
-
-                        <?php wc_get_template_part( 'content', 'product' ); ?>
-
-                    <?php endwhile; // end of the loop. ?>
-
-                    <?php woocommerce_product_loop_end(); ?>
-
-                </div>
-
-                <?php dokan_content_nav( 'nav-below' ); ?>
-
-            <?php } else { ?>
-
-            {{-- change: --}} 
-            @include('dokan.store-no-listings')
-
-            <?php } ?>
-            {{-- change: --}} 
-            {!! \Roots\view('partials.single-product-notice')->render() !!}
+  <div id="dokan-primary" class="dokan-single-store">
+    <div id="dokan-content" class="store-page-wrap woocommerce" role="main">
+      @php dokan_get_template_part( 'store-header' ) @endphp
+      @include('dokan.store-header-seller')
+      @php do_action( 'dokan_store_profile_frame_after', $store_user->data, $store_info ) @endphp
+      @if (have_posts())
+        <div class="seller-items">
+          @php woocommerce_product_loop_start() @endphp
+          @while (have_posts())
+            @php the_post() @endphp
+            @php wc_get_template_part( 'content', 'product' ) @endphp
+          @endwhile
+          @php woocommerce_product_loop_end() @endphp
         </div>
+        @php dokan_content_nav( 'nav-below' ) @endphp
+      @else
+        @include('dokan.store-no-listings')
+      @endif
+      {!! \Roots\view('partials.single-product-notice')->render() !!}
+    </div>
+  </div>
 
-    </div><!-- .dokan-single-store -->
+  @if ('right' === $layout)
+    @php
+      dokan_get_template_part('store', 'sidebar', [
+          'store_user' => $store_user,
+          'store_info' => $store_info,
+          'map_location' => $map_location,
+      ]);
+    @endphp
+  @endif
 
-    <?php if ( 'right' === $layout ) { ?>
-        <?php dokan_get_template_part( 'store', 'sidebar', array( 'store_user' => $store_user, 'store_info' => $store_info, 'map_location' => $map_location ) ); ?>
-    <?php } ?>
-
-</div><!-- .dokan-store-wrap -->
-
-<?php do_action( 'woocommerce_after_main_content' ); ?>
-
-<?php get_footer( 'shop' ); ?>
-
-
+</div>
+@php do_action( 'woocommerce_after_main_content' ) @endphp
+@php get_footer( 'shop' ) @endphp
