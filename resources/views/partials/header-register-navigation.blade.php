fcb876886d9a40630baf17d6d79f26878fa7b8c9
Alex Troyer <alex@espercreations.com>
2021-08-17 23:28:43 -0500

    Update register navigation style
    Added borders to link elements to avoid pixel spacing from adding border
    to parent list element

--- resources/views/partials/header-register-navigation.blade.php
+++ resources/views/partials/header-register-navigation.blade.php
@@ -1,12 +1,12 @@
 <nav class="h-full">
   <ul class="flex h-full">
-    <li class="h-full border-l border-r last:border-l-0">
+    <li class="h-full">
       <a href="{{ esc_url(wp_login_url(home_url())) }}"
-        class="h-full flex items-center text-sm font-bold relative uppercase px-3 lg:px-4 transition-colors hover:bg-gray-100">Login</a>
+        class="h-full border-l flex items-center text-sm font-bold relative uppercase px-3 lg:px-4 transition-colors hover:bg-gray-100">Login</a>
     </li>
-    <li class="h-full border-l border-r last:border-l-0">
+    <li class="h-full">
       <a href="{{ esc_url(wp_registration_url(home_url())) }}"
-        class="h-full flex items-center text-sm font-bold relative uppercase px-3 lg:px-4 transition-colors bg-gray-50 hover:bg-gray-100">Sign
+        class="h-full border-l border-r flex items-center text-sm font-bold relative uppercase px-3 lg:px-4 transition-colors bg-gray-50 hover:bg-gray-100">Sign
         Up</a>
     </li>
   </ul>
