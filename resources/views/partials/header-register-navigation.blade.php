fee1c6d543763040cfca3a845247a5082666e255
Alex Troyer <alex@espercreations.com>
2021-08-17 23:33:46 -0500

    Update header register navigation style
    Registration button to white background

--- resources/views/partials/header-register-navigation.blade.php
+++ resources/views/partials/header-register-navigation.blade.php
@@ -6,7 +6,7 @@
     </li>
     <li class="h-full">
       <a href="{{ esc_url(wp_registration_url(home_url())) }}"
-        class="h-full border-l border-r flex items-center text-sm font-bold relative uppercase px-3 lg:px-4 transition-colors bg-gray-50 hover:bg-gray-100">Sign
+        class="h-full border-l border-r flex items-center text-sm font-bold relative uppercase px-3 lg:px-4 transition-colors hover:bg-gray-100">Sign
         Up</a>
     </li>
   </ul>
