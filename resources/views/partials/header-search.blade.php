1e48735c4feb1c541f2f036bd4e72f182846c1d6
Alex Troyer <alex@espercreations.com>
2021-08-17 22:17:15 -0500

    Add responsive wrapper to search
    In order for the search dom node to fit in the header properly, a
    wrapper element with a defined max width outer spacing is required
    
    Adding a parent wrapper element with margin and max width makes the
    child search element responsive in size
    
    If the inner search element has additional spacing styling, the
    alignment of the element may conflict with other elements in the header

--- resources/views/partials/header-search.blade.php
+++ resources/views/partials/header-search.blade.php
@@ -1,3 +1,5 @@
-@if (is_active_sidebar('sidebar-header'))
+<div class="flex max-w-md w-full mx-4 lg:mx-8 lg:mr-0">
+  @if (is_active_sidebar('sidebar-header'))
     @php dynamic_sidebar('sidebar-header') @endphp
-@endif
+  @endif
+</div>
