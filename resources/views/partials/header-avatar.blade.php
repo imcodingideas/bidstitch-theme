b35af1e824bf82fb9dee427964edcd37c45fe2df
Alex Troyer <alex@espercreations.com>
2021-08-18 23:35:37 -0500

    Update header styles and sr sttributes
    - Add color transition to dropdown children on hover
    - Add margin between chevron icon and label on dropdown parent
    - Add container for mobile and desktop on header wrapper
    - Add SR attributes to SVG icons
    - Add SR label to header avatar placeholder

--- resources/views/partials/header-avatar.blade.php
+++ resources/views/partials/header-avatar.blade.php
@@ -2,6 +2,7 @@
   <a href="{{ get_permalink(get_option('woocommerce_myaccount_page_id')) }}"
     class="flex px-4 -mr-4 h-full items-center account-link account-login"
     title="{{ _e('My account', 'woocommerce') }}">
+    <span class="sr-only">Open user menu</span>
     <div class="h-8 w-8 rounded-full overflow-hidden bg-gray-200">
       @include('partials.header-avatar-image')
     </div>
