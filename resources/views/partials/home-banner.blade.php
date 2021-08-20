84d7beca4ee9b32acee47be0561221bc8968b6d6
Joseph Chambers <joseph@michael-chambers.com>
2021-08-20 14:00:24 -0500

    home-banner: check if image exists

--- resources/views/partials/home-banner.blade.php
+++ resources/views/partials/home-banner.blade.php
@@ -6,6 +6,7 @@
         @if ($slides)
           <div class="home-slider__slider">
             @foreach ($slides as $slide)
+              @if ($slide['image'])
               <div class="slider-home-page relative slider-{{ $slide['index'] }}" style="background-image: url('{{ $slide['image']['url'] }}');">
                 <img class="absolute h-full object-fill w-full" src="{{ $slide['image']['url'] }}" alt="slide" />
                 <div class="flex h-80 items-center justify-center relative">
@@ -19,6 +20,7 @@
                   @endif
                 </div>
               </div>
+              @endif
             @endforeach
           </div>
         @endif
