5665cc06df89c89b9e572e111e41ccc86f5aab24
Alex Troyer <alex@espercreations.com>
2021-08-22 14:40:11 -0500

    Add order item view composer

--- resources/views/woocommerce/myaccount/orders-item.blade.php
+++ resources/views/woocommerce/myaccount/orders-item.blade.php
@@ -1,5 +1,6 @@
-@php $order = wc_get_order($customer_order) @endphp
-<div class="grid gap-4 sm:gap-6">
-  @include('woocommerce.myaccount.orders-item-header')
-  @include('woocommerce.myaccount.orders-item-products')
-</div>
+@if ($order)
+  <div class="grid gap-4 sm:gap-6">
+    @include('woocommerce.myaccount.orders-item-header')
+    @include('woocommerce.myaccount.orders-item-products')
+  </div>
+@endif
