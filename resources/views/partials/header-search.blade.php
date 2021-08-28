9b3753b4863356a945dff875043b6f927b360424
Alex Troyer <alex@espercreations.com>
2021-08-28 16:19:22 -0500

    Update header search style and structure
    -- Change style to fit header navigation structure changes

--- resources/views/partials/header-search.blade.php
+++ resources/views/partials/header-search.blade.php
@@ -1,5 +1,16 @@
-<div class="flex max-w-md w-full mx-2 lg:mx-8 lg:mr-0">
-  @if (is_active_sidebar('sidebar-header'))
-    @php dynamic_sidebar('sidebar-header') @endphp
-  @endif
-</div>
+{{-- 
+@if (is_active_sidebar('sidebar-header'))
+  @php dynamic_sidebar('sidebar-header') @endphp
+@endif 
+--}}
+
+{{-- Placeholder search form --}}
+<form role="search" class="w-full h-full flex items-center overflow-hidden lg:border" method="get"
+  action="{{ esc_url(home_url('/')) }}">
+  <label class="flex pr-4 items-center justify-center h-full lg:p-2" for="search_input">
+    <img aria-hidden="true" focusable="false" class="flex opacity-60 w-5" src="@asset('images/search.svg')"
+      alt="search" />
+  </label>
+  <input id="search_input" class="w-full p-0 border-0 focus:ring-0 lg:p-2 lg:pl-0 lg:w-80" type="text"
+    placeholder="Search for items or sellers" />
+</form>
