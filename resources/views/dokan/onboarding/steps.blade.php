38b3f7d4ace5313920dc02b2d1739cf25878754a
Alex Troyer <alex@espercreations.com>
2021-09-01 19:51:33 -0500

    Update steps nav styling
    -- Update mobile styling to not have rounded corners on wrapper
    -- Updated mobile styling to expand full width of screen
    -- Centered child in each step

--- resources/views/dokan/onboarding/steps.blade.php
+++ resources/views/dokan/onboarding/steps.blade.php
@@ -1,17 +1,17 @@
 @if ($steps)
-  <div class="lg:border-b lg:border-gray-200">
-    <div class="container">
+  <div class="md:border-b md:border-gray-200">
+    <div
+      class="px-0 mx-auto max-w-full md:px-8 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
       <nav aria-label="Progress">
-        <ol role="list"
-          class="rounded-md overflow-hidden lg:flex lg:border-l lg:border-r lg:border-gray-200 lg:rounded-none">
+        <ol role="list" class="overflow-hidden md:flex md:border-l md:border-r md:border-gray-200">
           @foreach ($steps as $step_key => $step)
-            <li class="relative overflow-hidden lg:flex-1">
-              <div class="border border-gray-200 overflow-hidden border-b-0 rounded-t-md lg:border-0">
+            <li class="relative overflow-hidden md:flex-1">
+              <div class="border-b border-gray-200 overflow-hidden md:rounded-t-md md:border-b-0">
                 <div class="group">
                   <span
-                    class="absolute top-0 left-0 w-1 h-full lg:w-full lg:h-1 lg:bottom-0 lg:top-auto {{ $step->current ? 'bg-black' : 'bg-transparent' }}"
+                    class="absolute top-0 left-0 w-1 h-full md:w-full md:h-1 md:bottom-0 md:top-auto {{ $step->current ? 'bg-black' : 'bg-transparent' }}"
                     aria-hidden="true"></span>
-                  <span class="px-6 py-5 flex items-start text-sm font-medium">
+                  <span class="px-6 py-5 flex items-center text-sm font-medium">
                     <span class="flex-shrink-0">
                       <span
                         class="w-10 h-10 flex items-center justify-center rounded-full border-2 {{ $step->complete ? 'bg-black' : '' }} {{ $step->complete || $step->current ? 'border-black' : '' }}">
@@ -27,14 +27,15 @@
                         @endif
                       </span>
                     </span>
-                    <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
+                    <span class="ml-4 min-w-0 flex flex-col">
                       <span class="text-xs font-semibold tracking-wide uppercase">{{ $step->name }}</span>
-                      <span class="text-sm font-medium text-gray-500">{{ $step->description }}</span>
+                      <span
+                        class="text-xs font-medium text-gray-500 leading-none md:text-sm md:leading-none">{{ $step->description }}</span>
                     </span>
                   </span>
                 </div>
                 @if (!$loop->first)
-                  <div class="hidden absolute top-0 left-0 w-3 inset-0 lg:block" aria-hidden="true">
+                  <div class="hidden absolute top-0 left-0 w-3 inset-0 md:block" aria-hidden="true">
                     <svg class="h-full w-full text-gray-200" viewBox="0 0 12 82" fill="none" preserveAspectRatio="none">
                       <path d="M0.5 0V31L10.5 41L0.5 51V82" stroke="currentcolor" vector-effect="non-scaling-stroke" />
                     </svg>
