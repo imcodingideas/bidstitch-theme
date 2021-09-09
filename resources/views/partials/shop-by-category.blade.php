1e7781e2a55609159a55f89956f4d2d59c3f3f41
troyersolutions <54157027+troyersolutions@users.noreply.github.com>
2021-09-09 18:09:25 -0500

    Fix home categories conditional rendering
    -- Check if home categories variable exists before running the loop

--- resources/views/partials/shop-by-category.blade.php
+++ resources/views/partials/shop-by-category.blade.php
@@ -7,22 +7,24 @@
           href="{{ esc_url(wc_get_page_permalink('shop')) }}">{{ _e('Browse all categories', 'sage') }}</a>
       </div>
       <div class="grid grid-cols-2 gap-x-3 gap-y-3 md:gap-y-6 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-6">
-        @foreach ($categories as $category)
-          <a href="{{ esc_url($category->link) }}"
-            class="relative w-full h-48 md:h-72 rounded-lg p-6 flex flex-col overflow-hidden hover:opacity-75 xl:w-auto">
-            <span aria-hidden="true" class="absolute inset-0">
-              @if ($category->thumbnail)
-                <img src="{{ esc_url($category->thumbnail) }}" alt="" class="w-full h-full object-center object-cover">
-              @else
-                <span class="absolute bg-gray-200 h-full w-full p-8"></span>
-              @endif
-            </span>
-            <span aria-hidden="true"
-              class="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-70"></span>
-            <span
-              class="relative mt-auto text-center text-base font-bold text-white md:text-xl">{{ $category->name }}</span>
-          </a>
-        @endforeach
+        @if ($categories)
+          @foreach ($categories as $category)
+            <a href="{{ esc_url($category->link) }}"
+              class="relative w-full h-48 md:h-72 rounded-lg p-6 flex flex-col overflow-hidden hover:opacity-75 xl:w-auto">
+              <span aria-hidden="true" class="absolute inset-0">
+                @if ($category->thumbnail)
+                  <img src="{{ esc_url($category->thumbnail) }}" alt="" class="w-full h-full object-center object-cover">
+                @else
+                  <span class="absolute bg-gray-200 h-full w-full p-8"></span>
+                @endif
+              </span>
+              <span aria-hidden="true"
+                class="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-70"></span>
+              <span
+                class="relative mt-auto text-center text-base font-bold text-white md:text-xl">{{ $category->name }}</span>
+            </a>
+          @endforeach
+        @endif
       </div>
     </div>
   </div>
