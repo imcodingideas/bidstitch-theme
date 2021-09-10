ff72b02a211cfcaa44f66f710f2dd128aa9b1f12
Alex Troyer <alex@espercreations.com>
2021-09-10 15:43:56 -0500

    Update WC notice view/composer
    -- Added composer to product all notices
    -- Fetch session notices rather than all notices
    -- Clear notices to ensure that outdated notices aren't displayed

--- resources/views/woocommerce/notices.blade.php
+++ resources/views/woocommerce/notices.blade.php
@@ -1,5 +1,5 @@
-@if (wc_notice_count())
-  @foreach (wc_get_notices() as $type => $notices)
+@if ($notice_types)
+  @foreach ($notice_types as $type => $notices)
     @foreach ($notices as $notice)
       <x-alert type="{{ $type }}">
         {!! $notice['notice'] !!}
