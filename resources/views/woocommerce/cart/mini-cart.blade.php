9d104ca90d4d10f4e6453913bfdb16d0ad20c07e
Alex Troyer <alex@espercreations.com>
2021-09-17 18:53:00 -0500

    Add wrapping element to mini-cart to fix ajax
    -- Add WC mini-cart container class to fix mini-cart not updating on
    ajax call

--- resources/views/woocommerce/cart/mini-cart.blade.php
+++ resources/views/woocommerce/cart/mini-cart.blade.php
@@ -17,32 +17,34 @@
 * @version 5.2.0
 */
 @endphp
-@php do_action( 'woocommerce_before_mini_cart' ) @endphp
+<div class="widget_shopping_cart_content">
+  @php do_action( 'woocommerce_before_mini_cart' ) @endphp
 
-@if ($cart_empty)
+  @if ($cart_empty)
 
-  <p class="woocommerce-mini-cart__empty-message">{{ _e('No products in the cart.', 'woocommerce') }}</p>
+    <p class="woocommerce-mini-cart__empty-message">{{ _e('No products in the cart.', 'woocommerce') }}</p>
 
-@else
+  @else
 
-  <ul class="woocommerce-mini-cart cart_list flex flex-col space-y-2 {{ $args['list_class'] }}">
-    @php do_action( 'woocommerce_before_mini_cart_contents' ) @endphp
-    @foreach ($products as $product)
-      @include('woocommerce.cart.mini-cart-product')
-    @endforeach
-    @php do_action( 'woocommerce_mini_cart_contents' ) @endphp
-  </ul>
+    <ul class="woocommerce-mini-cart cart_list flex flex-col space-y-2 {{ $args['list_class'] }}">
+      @php do_action( 'woocommerce_before_mini_cart_contents' ) @endphp
+      @foreach ($products as $product)
+        @include('woocommerce.cart.mini-cart-product')
+      @endforeach
+      @php do_action( 'woocommerce_mini_cart_contents' ) @endphp
+    </ul>
 
-  <p class="border-b px-2 py-1 text-gray-800 text-lg text-right total woocommerce-mini-cart__total">
-    @php do_action( 'woocommerce_widget_shopping_cart_total' ) @endphp
-  </p>
+    <p class="border-b px-2 py-1 text-gray-800 text-lg text-right total woocommerce-mini-cart__total">
+      @php do_action( 'woocommerce_widget_shopping_cart_total' ) @endphp
+    </p>
 
-  @php do_action( 'woocommerce_widget_shopping_cart_before_buttons' ) @endphp
+    @php do_action( 'woocommerce_widget_shopping_cart_before_buttons' ) @endphp
 
-  <p class="woocommerce-mini-cart__buttons buttons flex flex-col items-center mt-3 space-y-2">@php do_action( 'woocommerce_widget_shopping_cart_buttons' )@endphp</p>
+    <p class="woocommerce-mini-cart__buttons buttons flex flex-col items-center mt-3 space-y-2">@php do_action( 'woocommerce_widget_shopping_cart_buttons' )@endphp</p>
 
-  @php do_action( 'woocommerce_widget_shopping_cart_after_buttons' ) @endphp
+    @php do_action( 'woocommerce_widget_shopping_cart_after_buttons' ) @endphp
 
-@endif
+  @endif
 
-@php do_action( 'woocommerce_after_mini_cart' ) @endphp
+  @php do_action( 'woocommerce_after_mini_cart' ) @endphp
+</div>
