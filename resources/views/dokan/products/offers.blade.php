ffb90803d3c7e7921a00700973e3f797aeb49bfc
Alex Troyer <alex@espercreations.com>
2021-09-17 17:19:28 -0500

    Update offer fields on create product page
    -- Force all products to have offers enabled by default
    -- Remove all other offer fields

---
+++ resources/views/dokan/products/offers.blade.php
@@ -0,0 +1,4 @@
+<div class="hidden">
+    <x-form-input type="hidden" id="offers_for_woocommerce_enabled" name="offers_for_woocommerce_enabled" value="yes"/>
+    <input type="hidden" name="ofw_dokan_product_nonce" value="{{ wp_create_nonce('ofw-dokan-product-nonce') }}">
+</div>
