1f923cf42db82364fa0e47f8c6c5444992061631
Joseph Chambers <joseph@michael-chambers.com>
2021-09-16 22:12:35 -0500

    fix: auctions without offers not shown in home
    changed query to show active auctions
    not closed and sorted by end date

--- app/View/Composers/AuctionsEndingSoon.php
+++ app/View/Composers/AuctionsEndingSoon.php
@@ -27,23 +27,32 @@ class AuctionsEndingSoon extends Composer
 
     public function args()
     {
+        // check auctions with dates between
+        // and not closed
+        $local_date = date_i18n('Y-m-d H:i');
         $args = [
             'post_type' => 'product',
             'post_status' => 'publish',
-            'posts_per_page' => 5,
-            'orderby' => 'meta_value_num',
+            'posts_per_page' => 4,
+            'orderby' => 'meta_value',
             'meta_key' => '_auction_dates_to',
             'order' => 'asc',
             'meta_query' => [
                 'relation' => 'AND',
                 [
-                    'key' => '_auction_current_bid',
-                    'compare' => 'EXISTS',
-                ],
-                [
                     'key' => '_auction_closed',
                     'compare' => 'NOT EXISTS',
                 ],
+                [
+                    'key' => '_auction_dates_from',
+                    'value' => $local_date,
+                    'compare' => '<',
+                ],
+                [
+                    'key' => '_auction_dates_to',
+                    'value' => $local_date,
+                    'compare' => '>',
+                ]
             ],
         ];
         return $args;
