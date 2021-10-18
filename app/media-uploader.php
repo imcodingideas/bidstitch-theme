45ddd2200ae3445dc95d1d988e2232d0bae08ddb
tinboyko-tt <valentin.boyko@toptal.com>
2021-10-18 16:41:00 +0500

    ajax error handling fixed

--- app/media-uploader.php
+++ app/media-uploader.php
@@ -67,13 +67,13 @@ add_action('wp_enqueue_scripts', function () {
  */
 add_action('wp_ajax_bidstitch_process_file_upload', function () {
 
-    if (!is_user_logged_in()) wp_send_json_error(esc_html__('Unauthorized', 'bidstitch'));
+    if (!is_user_logged_in()) wp_send_json_error(esc_html__('Unauthorized', 'bidstitch'), 400);
 
-    if (!wp_verify_nonce($_GET['nonce'], 'bidstitch_process_file_upload')) wp_send_json_error(esc_html__('Go away', 'bidstitch'));
+    if (!wp_verify_nonce($_GET['nonce'], 'bidstitch_process_file_upload')) wp_send_json_error(esc_html__('Go away', 'bidstitch'), 400);
 
-    if (!dokan_is_user_seller(get_current_user_id())) wp_send_json_error(esc_html__('Wrong permissions', 'bidstitch'));
+    if (!dokan_is_user_seller(get_current_user_id())) wp_send_json_error(esc_html__('Wrong permissions', 'bidstitch'), 400);
 
-    if (!isset($_FILES) || empty($_FILES)) wp_send_json_error(esc_html__('Please, upload file', 'bidstitch'));
+    if (!isset($_FILES) || empty($_FILES)) wp_send_json_error(esc_html__('Please, upload file', 'bidstitch'), 400);
 
     $file_key = sanitize_text_field(array_key_first($_FILES));
 
@@ -87,7 +87,9 @@ add_action('wp_ajax_bidstitch_process_file_upload', function () {
 
     $attachment_id = media_handle_upload($file_key, 0);
 
-    if (is_wp_error($attachment_id)) wp_send_json_error($attachment_id->get_error_message());
+    if (is_wp_error($attachment_id)) {
+        wp_send_json_error($attachment_id->get_error_message(), 400);
+    }
 
     wp_send_json($attachment_id);
 });
@@ -101,15 +103,15 @@ add_action('wp_ajax_bidstitch_revert_file_upload', function () {
 
     if (!is_user_logged_in()) wp_send_json_error(esc_html__('Unauthorized', 'bidstitch'));
 
-    if (!wp_verify_nonce($_GET['nonce'], 'bidstitch_revert_file_upload')) wp_send_json_error(esc_html__('Go away', 'bidstitch'));
+    if (!wp_verify_nonce($_GET['nonce'], 'bidstitch_revert_file_upload')) wp_send_json_error(esc_html__('Go away', 'bidstitch'), 400);
 
     $current_user_id = get_current_user_id();
 
-    if (!dokan_is_user_seller($current_user_id)) wp_send_json_error(esc_html__('Wrong permissions', 'bidstitch'));
+    if (!dokan_is_user_seller($current_user_id)) wp_send_json_error(esc_html__('Wrong permissions', 'bidstitch'), 400);
 
     $attachment_id = intval(file_get_contents('php://input'));
     $author_id = intval(get_post_field('post_author', $attachment_id));
-    if ($author_id !== $current_user_id) wp_send_json_error(esc_html__('Insufficient permission', 'bidstitch'));
+    if ($author_id !== $current_user_id) wp_send_json_error(esc_html__('Insufficient permission', 'bidstitch'), 400);
 
     /*Now we can delete uploaded image*/
     wp_delete_attachment($attachment_id);
