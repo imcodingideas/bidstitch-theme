32762e4efc0ab67b2bb152e1ddb064bacc966ba1
Robert Davies <robert@theriftlab.com>
2022-01-19 12:37:37 -0800

    Tentative front-end/copy changes.

--- resources/views/archive-event.blade.php
+++ resources/views/archive-event.blade.php
@@ -3,18 +3,18 @@
 @section('content')
   @if ($events['partnered'] || $events['external'])
 
-    <div x-data="{ tab: 'partnered' }">
+    <div x-cloak x-data="{ tab: 'partnered' }">
       <div class="mb-8 flex border-b border-gray-300">
-        <div x-on:click="tab = 'partnered'" class="w-1/2 sm:w-auto border border-gray-300 border-b-0 border-r-0 px-8 py-2 text-center cursor-pointer" :class="tab === 'partnered' ? '' : 'bg-gray-100 text-gray-500'">
-          BidStitch
+        <div x-on:click="tab = 'partnered'" class="w-1/2 sm:w-auto border border-gray-300 border-b-0 border-r-0 px-8 py-2 text-center leading-tight cursor-pointer" :class="tab === 'partnered' ? 'bg-white text-black' : 'bg-gray-100 text-gray-500'">
+          Partnered Events
         </div>
-        <div x-on:click="tab = 'external'" class="w-1/2 sm:w-auto border border-gray-300 border-b-0 px-8 py-2 text-center cursor-pointer" :class="tab === 'external' ? '' : 'bg-gray-100 text-gray-500'">
-          Shoutouts
+        <div x-on:click="tab = 'external'" class="w-1/2 sm:w-auto border border-gray-300 border-b-0 px-8 py-2 text-center leading-tight cursor-pointer" :class="tab === 'external' ? 'bg-white text-black' : 'bg-gray-100 text-gray-500'">
+          Featured Events
         </div>
       </div>
 
       <div x-show="tab === 'partnered'">
-        <h1 class="mb-6 text-xl text-center font-bold">BidStitch Partner Events</h1>
+        <h1 class="mb-6 text-xl text-center font-bold">BIDSTITCH Partner Events</h1>
 
         @if ($events['partnered'])
           <div class="bg-white overflow-hidden sm:rounded-md">
@@ -32,7 +32,7 @@
       </div>
 
       <div x-cloak x-show="tab === 'external'">
-        <h1 class="mb-6 text-xl text-center font-bold">Other Events</h1>
+        <h1 class="mb-6 text-xl text-center font-bold">Featured Events</h1>
 
         @if ($events['external'])
           <div class="bg-white shadow overflow-hidden sm:rounded-md">
