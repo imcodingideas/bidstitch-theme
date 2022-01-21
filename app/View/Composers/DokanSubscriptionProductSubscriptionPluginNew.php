61d5b162d06681e5ce24369b3d181d47c47f7f39
Robert Davies <robert@theriftlab.com>
2022-01-20 16:22:02 -0800

    Tidied up front-end on subscriptions page & added card number last 4 digits.

--- app/View/Composers/DokanSubscriptionProductSubscriptionPluginNew.php
+++ app/View/Composers/DokanSubscriptionProductSubscriptionPluginNew.php
@@ -2,10 +2,17 @@
 
 namespace App\View\Composers;
 
+use Illuminate\View\View;
 use Roots\Acorn\View\Composer;
+use Stripe\StripeClient;
+use WeDevs\DokanPro\Modules\Stripe\Helper as StripeHelper;
 
 class DokanSubscriptionProductSubscriptionPluginNew extends Composer
 {
+    protected $customer_id;
+    protected $subscription_id;
+    protected $has_subscription;
+
     /**
      * List of views served by this composer.
      *
@@ -14,27 +21,60 @@ class DokanSubscriptionProductSubscriptionPluginNew extends Composer
     protected static $views = ['dokan.subscription.product_subscription_plugin_new'];
 
     /**
+     * Compose the view before rendering.
+     *
+     * @param  View $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $user_id = get_current_user_id();
+        $customer_id_meta = get_user_meta($user_id, 'dokan_stripe_customer_id');
+        $subscription_id_meta = get_user_meta($user_id, '_stripe_subscription_id');
+
+        $this->has_subscription = !empty($customer_id_meta) && !empty($subscription_id_meta);
+
+        if ($this->has_subscription) {
+            $this->customer_id = $customer_id_meta[0];
+            $this->subscription_id = $subscription_id_meta[0];
+        }
+
+        parent::compose($view);
+    }
+
+    /**
      * Data to be passed to view before rendering.
      *
      * @return array
      */
     public function with()
     {
+        $card_last_digits = $this->card_last_digits();
         $display_stripe_button = $this->display_stripe_button();
         $display_success_message = $this->display_success_message();
 
         return [
-            'display_stripe_button' => !$display_success_message && $display_stripe_button,
+            'card_last_digits' => $card_last_digits,
+            'display_stripe_button' => $display_stripe_button,
             'display_success_message' => $display_success_message,
         ];
     }
 
+    protected function card_last_digits()
+    {
+        $stripe = new StripeClient(StripeHelper::get_secret_key());
+
+        $payment_methods = $stripe->paymentMethods->all([
+            'customer' => $this->customer_id,
+            'type' => 'card',
+        ]);
+
+        return $payment_methods->data[0]->card->last4;
+    }
+
     protected function display_stripe_button()
     {
-        $user_id = get_current_user_id();
-        $customer_id_meta = get_user_meta($user_id, 'dokan_stripe_customer_id');
-        $subscription_id_meta = get_user_meta($user_id, '_stripe_subscription_id');
-        return !empty($customer_id_meta) & !empty($subscription_id_meta);
+        return $this->has_subscription;
     }
 
     protected function display_success_message()
--- resources/views/dokan/subscription/product_subscription_plugin_new.blade.php
+++ resources/views/dokan/subscription/product_subscription_plugin_new.blade.php
@@ -12,15 +12,22 @@
     <div>
       {!! do_shortcode('[dps_product_pack]') !!}
 
-      @if ($display_stripe_button)
-        <div class="-mt-8 mb-6">
-          <a class="inline-block w-full sm:w-auto px-6 py-3 bg-black text-white" href="/update-stripe-payment-details">Update Card Details</a>
-        </div>
-      @elseif ($display_success_message)
-        <div class="-mt-8 mb-6">
-          <div class="inline-block px-6 py-3 border border-green-500 bg-green-100">
-            Your card details have been successfully updated!
-          </div>
+      @if ($display_stripe_button || $display_success_message)
+        <div class="-mt-8 mb-6 space-y-3">
+
+          @if ($display_success_message)
+            <div class="px-6 py-3 border border-green-500 bg-green-100 text-center">
+              Your card details have been successfully updated!
+            </div>
+          @endif
+
+          @if ($display_stripe_button)
+            <div class="bg-white shadow-lg p-4 flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
+              <p>Payment method: <span class="font-mono text-gray-700">XXXX-{{ $card_last_digits }}</span></p>
+              <a class="inline-block w-full sm:w-auto px-6 py-3 bg-black text-white text-center" href="/update-stripe-payment-details">Update Card Details</a>
+            </div>
+          @endif
+
         </div>
       @endif
     </div>
