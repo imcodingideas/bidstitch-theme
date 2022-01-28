7aaa8047baaa91047ebe9ca80998a9cdf0fb104e
Robert Davies <robert@theriftlab.com>
2022-01-28 13:37:06 -0800

    Fixed blog page search bar stretch on Safari.

--- resources/views/forms/search-archive.blade.php
+++ resources/views/forms/search-archive.blade.php
@@ -1,9 +1,9 @@
-<form role="search" class="w-full h-full flex items-center border border-gray-300 bg-white relative px-2" method="get"
+<form role="search" class="w-full flex items-center border border-gray-300 bg-white relative px-2" method="get"
   action="{{ esc_url(get_permalink(get_option('page_for_posts'))) }}">
   <label class="flex pr-2 items-center justify-center h-full md:p-2 md:pr-4" for="search_input">
     <img aria-hidden="true" focusable="false" class="flex opacity-60 w-5" src="@asset('images/search.svg')"
       alt="search" />
   </label>
   <input required class="w-full p-0 border-0 focus:ring-0 text-sm md:p-2 md:pl-0 md:text-base" name="s" id="search_input"
-    class="" type=" search" placeholder="Search for articles" />
+    type="search" placeholder="Search for articles" />
 </form>
