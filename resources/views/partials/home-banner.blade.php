77e8c484378eb5a2f641c7cb9be7655e5bf648f0
Robert Davies <robert@theriftlab.com>
2022-05-17 12:54:20 -0700

    Homepage sliders now uniform height.

--- resources/views/partials/home-banner.blade.php
+++ resources/views/partials/home-banner.blade.php
@@ -5,7 +5,7 @@
       <div class="home-slider__slider relative">
         @foreach ($slides as $slide)
           {{-- Empty wrapping element for slick slider --}}
-          <div style="background-image:url('{{ $slide['image']['url'] }} ');">
+          <div class="h-full" style="background-image:url('{{ $slide['image']['url'] }} ');">
             <div class="container items-center">
               <div
                 class="col-span-8 row-start-2 lg:row-start-auto lg:col-span-5 flex flex-col justify-center items-center space-y-5 p-8 md:px-0">
