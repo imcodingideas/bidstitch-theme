d5f4c547c39948d39ac0f7ceecafca12b88b43d7
Robert Davies <robert@theriftlab.com>
2022-05-18 14:39:27 -0700

    Reverted homepage slider to old style.

--- resources/views/partials/home-banner.blade.php
+++ resources/views/partials/home-banner.blade.php
@@ -1,10 +1,10 @@
-<section id="home_banner">
+{{-- <section id="home_banner">
   <div
     class="">
     @if ($slides)
       <div class="home-slider__slider relative">
         @foreach ($slides as $slide)
-          {{-- Empty wrapping element for slick slider --}}
+          {{-- Empty wrapping element for slick slider --}
           <div class="h-full bg-cover bg-center" style="background-image:url('{{ $slide['image']['url'] }} ');">
             <div class="container items-center">
               <div
@@ -15,6 +15,39 @@
                 @if ($slide['button'])
                   <a href="{{ esc_attr($slide['link']) }}" class="btn btn--white btn--md capitalize font-400">
                     {{ $slide['button'] }}</a>
+                @endif
+              </div>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    @endif
+  </div>
+</section> --}}
+
+<section id="home_banner">
+  <div
+    class="px-0 mx-auto max-w-full lg:py-8 sm:px-8 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+    @if ($slides)
+      <div class="home-slider__slider relative">
+        @foreach ($slides as $slide)
+          {{-- Empty wrapping element for slick slider --}}
+          <div>
+            <div class="grid grid-cols-12 gap-x-8 items-center">
+              <div
+                class="col-span-12 row-start-2 lg:row-start-auto lg:col-span-5 flex flex-col justify-center items-start space-y-5 p-8 md:px-0">
+                <div class="grid space-y-3">
+                  {!! $slide['content'] !!}
+                </div>
+                @if ($slide['button'])
+                  <a href="{{ esc_attr($slide['link']) }}" class="btn btn--white btn--md">
+                    {{ $slide['button'] }}</a>
+                @endif
+              </div>
+              <div
+                class="col-span-12 row-start-1 lg:row-start-auto lg:col-span-7 overflow-hidden relative w-full aspect-w-16 aspect-h-9 bg-gray-100">
+                @if ($slide['image'])
+                  <img class="h-full w-full object-cover" src="{{ $slide['image']['url'] }}" alt="slide" />
                 @endif
               </div>
             </div>
