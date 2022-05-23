27e658099a7cf0b948dd03c765c25e164dadf3f9
Robert Davies <robert@theriftlab.com>
2022-05-23 16:13:35 -0700

    Revert "Rearranged homepage elements."
    This reverts commit fa6034c9f1627bc51477d96d37bb966d07a16e6a.

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -3,10 +3,10 @@
 @section('content')
   @while(have_posts()) @php(the_post())
   @include('partials.home-banner')
-  @include('partials.featured-articles')
+  @include('partials.shop-by-category')
   @include('partials.listings-of-the-week')
   @include('partials.sellers-of-the-week')
-  @include('partials.shop-by-category')
+  @include('partials.featured-articles')
   @include('partials.single-product-notice')
   @endwhile
 @endsection
